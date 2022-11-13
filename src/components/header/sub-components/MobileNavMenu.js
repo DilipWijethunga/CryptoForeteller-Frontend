9b38fc9d2931f05b40cd56f0d09c699b8a660706
DilipWijethunga <it19240466@my.sliit.lk>
2022-11-13 15:00:13 +0530

    header change

--- src/components/header/sub-components/MobileNavMenu.js
+++ src/components/header/sub-components/MobileNavMenu.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
+import UserService from "../../../services/UserService";
 
 const MobileNavMenu = () => {
+
+  const [user, setUser] = useState(null);
+  const [showUserBoard, setShowUserBoard] = useState(false);
+  const [showAdminBoard, setShowAdminBoard] = useState(false);
+
+  useEffect(() => {
+    const user = UserService.getCurrentUser();
+    if (user) {
+      setUser(user.data.username);
+      if (user.data.role === "Admin") {
+        setShowAdminBoard(user.data.role);
+      } else if (user.data.role === "Customer") {
+        setShowUserBoard(user.data.role);
+      }
+    }
+  }, []);
+
   return (
     <nav className="offcanvasNavigation" id="offcanvas-navigation">
       <ul>
@@ -19,9 +37,23 @@ const MobileNavMenu = () => {
         <li>
           <Link to={process.env.PUBLIC_URL + "/contact"}>Contact</Link>
         </li>
-        <li className="menuItemHasChildren">
-          <Link to={process.env.PUBLIC_URL + "/login"}>Login / Register</Link>
-        </li>
+
+        {user ? (
+            <li className="menuItemHasChildren">
+              {/*<Link to={process.env.PUBLIC_URL + "/login-register"}>Sign up</Link>*/}
+              {showUserBoard && (
+                  <Link to={process.env.PUBLIC_URL + "/customer-dashboard"}>Dashboard</Link>
+              )}
+              {showAdminBoard && (
+                  <Link to={process.env.PUBLIC_URL + "/admin-dashboard"}>Dashboard</Link>
+              )}
+              <Link to={process.env.PUBLIC_URL + "/login"} onClick={UserService.logout}>Logout</Link>
+            </li>
+        ) : (
+            <li className="menuItemHasChildren">
+              <Link to={process.env.PUBLIC_URL + "/login"}>Login / Register</Link>
+            </li>
+        )}
       </ul>
     </nav>
   );
